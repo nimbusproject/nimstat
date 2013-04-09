6596be54b3941456a743487673f4e39a84ea4c2f
Pierre Riteau <priteau@ci.uchicago.edu>
2013-04-09 15:19:32 +0100

    This is nimstat, not cloudinit.d

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Version = "0.1"
 
 if float("%d.%d" % sys.version_info[:2]) < 2.5:
     sys.stderr.write("Your Python version %d.%d.%d is not supported.\n" % sys.version_info[:3])
-    sys.stderr.write("cloudinitd requires Python 2.5 or newer.\n")
+    sys.stderr.write("nimstat requires Python 2.5 or newer.\n")
     sys.exit(1)
 
 setup(name='nimstat',
